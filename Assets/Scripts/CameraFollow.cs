700687ff49dd1773d569f418264ea06ed9325734
chris-gong <chris_gong@yahoo.com>
2019-05-28 21:39:12 -0400

    uploading initial project with basic player and camera movement controlled by joystick controls

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform target;
+
+    // the higher this value is, the faster it will lock on to the target, the lower this value is, the more time it will smooth
+    public float smoothSpeed = 0.5f;
+    public Vector3 offset;
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        Vector3 newPosition = target.position + offset;
+        Vector3 smoothedPosition = Vector3.Slerp(transform.position, newPosition, smoothSpeed * Time.deltaTime);
+        transform.position = newPosition;
+
+        transform.LookAt(target);
+    }
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed = 5f;
+    private Rigidbody rigidbody;
+
+    void Awake()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void FixedUpdate()
+    {
+        if (Input.GetButton("Accelerate"))
+        {
+            print("a button held down " + Input.GetAxis("Horizontal"));
+            Vector3 force = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical")) * speed;
+            rigidbody.AddForce(force);
+        }
+    }
+}
